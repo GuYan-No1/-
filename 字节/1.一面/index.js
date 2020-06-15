dceac66698d2a84cc63438d20733247f6a6a5328
王闯 <865748877@qq.com>
2020-06-15 14:20:32 +0800

    chore:新增算法目录

--- 字节/1.一面/index.js
+++ 字节/1.一面/index.js
@@ -1,7 +1,14 @@
-const arr = [1, 2, 3, 4, 5, 6];
-const obj = {
-  a: 'a'
+Array.prototype.forEach = function (cb, context) {
+  if (!this instanceof Array || typeof cb !== 'function') return;
+  let arr = Array.prototype.slice.call(this);
+  for (let i = 0; i < arr.length; i++) {
+    if (!arr[i]) continue;
+    cb.call(context, arr[i], i, arr);
+  }
 }
-arr.forEach(function () {
-  console.log('this', this);
-}, obj)
+const a = [1, 2, 3, , 4, , 5];
+let index = 0;
+const result = a.forEach(() => {
+  index++;
+});
+
